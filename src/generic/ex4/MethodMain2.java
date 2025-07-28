2cc1e83cb4ae4a0e179d8d19caa37c8a5db5b4ee
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-28 20:46:50 +0900

    add: MethodMain3

--- src/generic/ex4/MethodMain2.java
+++ src/generic/ex4/MethodMain2.java
@@ -11,5 +11,9 @@ public class MethodMain2 {
 
         AnimalMethod.checkup(dog);
         AnimalMethod.checkup(cat);
+
+        Dog targetDog = new Dog("큰 멍멍이", 200);
+        Dog bigger = AnimalMethod.bigger(dog, targetDog);
+        System.out.println("bigger = " + bigger);
     }
 }
