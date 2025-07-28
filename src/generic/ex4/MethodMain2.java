059432b3774367541a7d0a9163213898ba0e861a
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-28 20:35:10 +0900

    add: MethodMain2

---
+++ src/generic/ex4/MethodMain2.java
@@ -0,0 +1,15 @@
+package generic.ex4;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class MethodMain2 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("멍멍이", 100);
+        Cat cat = new Cat("냐옹이", 100);
+
+        AnimalMethod.checkup(dog);
+        AnimalMethod.checkup(cat);
+    }
+}
