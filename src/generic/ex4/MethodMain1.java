94b615261000b2a9aff3e4f22e0fa7dfd5e03d98
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-28 18:45:22 +0900

    add: MethodMain1

---
+++ src/generic/ex4/MethodMain1.java
@@ -0,0 +1,15 @@
+package generic.ex4;
+
+public class MethodMain1 {
+
+    public static void main(String[] args) {
+        Integer i = 10;
+        Object object = GenericMethod.objMethod(i);
+
+        System.out.println("명시적 타입 인자 전달");
+        Integer result = GenericMethod.<Integer>genericMethod(i);
+        System.out.println("result = " + result);
+        Integer integerValue = GenericMethod.<Integer>numberMethod(10);
+        Double doubleValue = GenericMethod.<Double>numberMethod(20.0);
+    }
+}
