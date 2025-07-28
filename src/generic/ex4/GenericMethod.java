5cec873a6c8829261f9cbd378f9cfa4e2be6a30d
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-28 18:40:39 +0900

    add: GenericMethod

---
+++ src/generic/ex4/GenericMethod.java
@@ -0,0 +1,14 @@
+package generic.ex4;
+
+public class GenericMethod {
+
+    public static Object objMethod(Object obj) {
+        System.out.println("obj = " + obj);
+        return obj;
+    }
+
+    public static <T> T genericMethod(T obj) {
+        System.out.println("obj = " + obj);
+        return obj;
+    }
+}
