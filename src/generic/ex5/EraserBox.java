645c144957b6561b827ae60ba45585ecdaf33d14
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-31 11:46:40 +0900

    add: EraserBox

---
+++ src/generic/ex5/EraserBox.java
@@ -0,0 +1,14 @@
+package generic.ex5;
+
+public class EraserBox <T> {
+
+
+    public boolean instaceCheck(Object param) {
+        //return param instanceof T;
+        return false;
+    }
+
+    public void create() {
+//        return new T();
+    }
+}
