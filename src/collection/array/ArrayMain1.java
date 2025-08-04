2b2ca1f512f09c1e1ca2f749390ed8db6d6b8a81
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-04 18:46:41 +0900

    adit: ArrayMain1

--- src/collection/array/ArrayMain1.java
+++ src/collection/array/ArrayMain1.java
@@ -7,14 +7,13 @@ public class ArrayMain1 {
     public static void main(String[] args) {
 
         int[] arr = new int[5];
-        //index 입력: O(1)
+
         System.out.println("==index 입력: O(1)==");
         arr[0] = 1;
         arr[1] = 2;
         arr[2] = 3;
         System.out.println(Arrays.toString(arr));
 
-        //index 변경: O(1)
         System.out.println("==index 변경: O(1)==");
         arr[2] = 10;
         System.out.println(Arrays.toString(arr));
