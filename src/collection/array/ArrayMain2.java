b9296204ead19db708c1439bffb71052a7c7c88d
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-05 15:17:14 +0900

    add: ArrayMain2

---
+++ src/collection/array/ArrayMain2.java
@@ -0,0 +1,13 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain2 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        System.out.println("arr = " + Arrays.toString(arr));
+    }
+}
