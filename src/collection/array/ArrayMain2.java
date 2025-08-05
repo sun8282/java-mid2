030f880a372583f0121e993ab3f699bda1c4d598
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-05 15:23:55 +0900

    add: ArrayMain2

--- src/collection/array/ArrayMain2.java
+++ src/collection/array/ArrayMain2.java
@@ -13,6 +13,18 @@ public class ArrayMain2 {
         int newValue = 3;
         addFirst(arr, newValue);
         System.out.println(Arrays.toString(arr));
+
+        int index = 2;
+        int value = 4;
+
+        addAtIndex(arr, index, value);
+    }
+
+    private static void addAtIndex(int[] arr, int index, int value) {
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i -1];
+        }
+        arr[index] = value;
     }
 
     private static void addFirst(int[] arr, int newValue) {
