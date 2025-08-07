6e3f4830152141c2800ee3e62397624fa23cd49d
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-07 11:32:55 +0900

    add: MyArrayListV1Main

---
+++ src/collection/array/MyArrayListV1Main.java
@@ -0,0 +1,10 @@
+package collection.array;
+
+public class MyArrayListV1Main {
+
+    public static void main(String[] args) {
+        MyArrayListV1 list = new MyArrayListV1();
+        System.out.println("데이터를 추가");
+        System.out.println(list);
+    }
+}
