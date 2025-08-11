2ef28bacc5967aac5721b3020b3a7fee9abd9c43
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-11 15:16:19 +0900

    edit: Node

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -9,11 +9,28 @@ public class Node {
         this.item = item;
     }
 
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
+
+
     @Override
     public String toString() {
-        return "Node{" +
-                "item=" + item +
-                ", next=" + next +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
