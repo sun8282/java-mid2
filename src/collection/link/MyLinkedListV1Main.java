f6ae2c7e163a9651a7b0c670520ba76ce4beaec7
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-12 15:25:00 +0900

    add: MyLinkedListV1Main

---
+++ src/collection/link/MyLinkedListV1Main.java
@@ -0,0 +1,13 @@
+package collection.link;
+
+
+
+public class MyLinkedListV1Main {
+
+    public static void main(String[] args) {
+        MyLinkedListV1 list = new MyLinkedListV1();
+        System.out.println("데이터를 추가");
+        System.out.println(list);
+
+    }
+}
