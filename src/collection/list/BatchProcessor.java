752173269fb4dcbc6a81be6b3205e3aae1ea2886
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-14 10:50:38 +0900

    add: BatchProcessor

--- src/collection/list/BatchProcessor.java
+++ src/collection/list/BatchProcessor.java
@@ -9,8 +9,11 @@ public class BatchProcessor {
     }
 
     public void logic(int size) {
+        long startTime = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
             list.add(0, i);
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("크기: " + size + ", 계산 시간: " + (endTime - startTime) + "ms");
     }
 }
