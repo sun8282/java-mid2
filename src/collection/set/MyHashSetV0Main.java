a33c60294b9e80e1f4a76c88f4554c806a47dfa7
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-21 11:19:38 +0900

    add: MyHashSetV0Main

---
+++ src/collection/set/MyHashSetV0Main.java
@@ -0,0 +1,13 @@
+package collection.set;
+
+public class MyHashSetV0Main {
+    public static void main(String[] args) {
+        MyHashSetV0 set = new MyHashSetV0();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);
+        System.out.println("set = " + set);
+    }
+}
