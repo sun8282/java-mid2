2c82928c340cbf8e760c0fffa22fbbf06fc8dba0
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-22 15:06:41 +0900

    add: HashStart5

--- src/collection/set/HashStart5.java
+++ src/collection/set/HashStart5.java
@@ -1,10 +1,17 @@
 package collection.set;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class HashStart5 {
 
     static final int CAPACITY = 10;
 
     public static void main(String[] args) {
-
+        LinkedList<Integer>[] buckets = new LinkedList[CAPACITY];
+        System.out.println("buckets = " + Arrays.toString(buckets));
+        for (int i = 0; i < CAPACITY; i++) {
+            buckets[i] = new LinkedList<>();
+        }
     }
 }
