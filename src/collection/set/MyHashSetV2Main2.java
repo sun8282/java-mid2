b9ede20b9dfc3b2b9cb7ce0a779a85abadc03f6e
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-26 17:02:02 +0900

    add: MyHashSetV2Main2

---
+++ src/collection/set/MyHashSetV2Main2.java
@@ -0,0 +1,17 @@
+package collection.set;
+
+public class MyHashSetV2Main2 {
+
+    public static void main(String[] args) {
+        MyHashSetV2 set = new MyHashSetV2(10);
+        Member hi = new Member("hi");
+        Member jpa = new Member("JPA");
+        Member java = new Member("java");
+        Member spring = new Member("spring");
+
+        System.out.println("hi.hashCode() = " + hi.hashCode());
+        System.out.println("jpa.hashCode() = " + jpa.hashCode());
+        System.out.println("java.hashCode() = " + java.hashCode());
+        System.out.println("spring.hashCode() = " + spring.hashCode());
+    }
+}
