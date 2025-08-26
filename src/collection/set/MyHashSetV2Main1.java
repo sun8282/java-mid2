ea38fed4965aea1ccca71aff8357e014110ca6a2
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-08-26 16:58:32 +0900

    add: MyHashSetV2Main1

---
+++ src/collection/set/MyHashSetV2Main1.java
@@ -0,0 +1,17 @@
+package collection.set;
+
+public class MyHashSetV2Main1 {
+
+    public static void main(String[] args) {
+        MyHashSetV2 set = new MyHashSetV2(10);
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        set.add("D");
+        set.add("AB");
+        set.add("SET");
+        System.out.println("set = " + set);
+
+        System.out.println("\"A\".hashCode() = " + "A".hashCode());
+    }
+}
