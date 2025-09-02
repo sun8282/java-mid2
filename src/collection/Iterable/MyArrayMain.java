3d6488f42b434154d00ed3772d65e3dd37f0ac22
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-09-02 18:26:39 +0900

    add: MyArrayMain

---
+++ src/collection/Iterable/MyArrayMain.java
@@ -0,0 +1,15 @@
+package collection.Iterable;
+
+import java.util.Iterator;
+
+public class MyArrayMain {
+
+    public static void main(String[] args) {
+        MyArray myArray = new MyArray(new int[]{1, 2, 3, 4});
+        Iterator<Integer> iterator = myArray.iterator();
+       while(iterator.hasNext()) {
+           Integer value = iterator.next();
+           System.out.println("value = " + value);
+       }
+    }
+}
