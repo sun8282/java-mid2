e39d8b7987a898db330197eba54272e68e4ee228
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-09-03 14:56:50 +0900

    add: SortMain1

---
+++ src/collection/compare/SortMain1.java
@@ -0,0 +1,14 @@
+package collection.compare;
+
+import java.util.Arrays;
+
+public class SortMain1 {
+
+    public static void main(String[] args) {
+        Integer[] array = {3, 2, 1};
+        System.out.println(Arrays.toString(array));
+
+        Arrays.sort(array);
+        System.out.println("array = " + Arrays.toString(array));
+    }
+}
